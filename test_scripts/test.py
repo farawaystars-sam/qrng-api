0d45544566388f78c32c31e6c1df84b64fd11e0c
farawaystars-sam <samhitaa1@gmail.com>
2024-07-26 23:28:19 +0530

    added the deployed url for testing

--- test_scripts/test.py
+++ test_scripts/test.py
@@ -2,7 +2,11 @@
 import requests, json
 
 cases = [1, 10, 20, 100, 10000]
-local_host = "http://127.0.0.1:5000"
+# the url for running on local machine, to test uncomment the lines with *
+#*local_host = "http://127.0.0.1:5000"
+
+# the url provided by the GCP cloud run service
+cloud_host = "https://qrng-api-v001-wmoc4p3wpa-uc.a.run.app"
 end_point="/random_bit_string"
 responses = {}
 count = 0
@@ -11,7 +15,10 @@ for n in cases:
     params ={
         'n':n
     }
-    res = requests.get(local_host+end_point, params=params)
+
+    #*res = requests.get(local_host+end_point, params=params)
+    res = requests.get(cloud_host+end_point, params=params)
+
     if n == len(json.loads(res.text)['bit_string']):
         responses[n] = "OK"
         count += 1
--- test_scripts/test1.py
+++ test_scripts/test1.py
@@ -1,8 +1,13 @@
 # testing the api endpoint /random_bit_string
 import requests, json
 
-cases = [(3, None), (10, 1), (100, 10)]
-local_host = "http://127.0.0.1:5000"
+cases = [(10, 1), (100, 10)]
+
+# the url for running on local machine, to test uncomment the lines with *
+#*local_host = "http://127.0.0.1:5000"
+
+# the url provided by the GCP cloud run service
+cloud_host = "https://qrng-api-v001-wmoc4p3wpa-uc.a.run.app"
 end_point="/random_int"
 responses = {}
 count = 0
@@ -13,7 +18,8 @@ for max, min in cases:
         'min': min
     }
     print(params)
-    res = requests.get(local_host+end_point, params=params)
+    #*res = requests.get(local_host+end_point, params=params)
+    res = requests.get(cloud_host+end_point, params=params)
     res = json.loads(res.text)
     print(res)
     if min <= res['rand_int'] and max > res['rand_int']:
